bec6b3244155a69e04ffb76c3dedfd2414a2c5c6
Bratislav <bratislav@Linux.com>
2014-12-03 22:42:52 -0500

    Inital upload of the project

---
+++ dist-sort.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import sys
+import csv
+
+reader = csv.reader(sys.stdin, delimiter=';')
+
+
+for line in reader:
+    #Try to interpret 3rd column as a float. Ignore line on error.
+    try:
+        power = float(line[2])
+    except:
+        continue
+    # Key = power, value = entire line
+    print power, "\t", writer.writerow(line)
+
